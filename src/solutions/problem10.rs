74edf1d61d53b346fff8513bbf4914caf5ae7cc3
worldmaker <worldmaker18349276@gmail.com>
2022-02-19 14:21:33 +0800

    solve problem10 by DP tabulation

--- src/solutions/problem10.rs
+++ src/solutions/problem10.rs
@@ -56,3 +56,45 @@ impl Problem10 for Solution {
         is_match_iter(s.chars().rev(), p.chars().rev())
     }
 }
+
+
+struct SolutionDP;
+
+impl Problem10 for SolutionDP {
+    fn is_match(s: String, p: String) -> bool {
+        let p = p.chars().collect::<Vec<_>>();
+        let plen = p.len();
+
+        let mut dp = vec![false; p.len()+1];
+
+        // match empty string
+        dp[0] = true;
+        for (i, a) in p.iter().enumerate() {
+            match a {
+                '*' => dp[i+1] = dp[i-1],
+                _ => dp[i+1] = false,
+            }
+        }
+
+        for ch in s.chars() {
+            // match `ch` additionally
+            let dp_prev = dp.clone();
+            dp[0] = false;
+            for (i, a) in p.iter().enumerate() {
+                match a {
+                    '*' => {
+                        dp[i+1] = dp[i-1] || dp[i];
+                        if ch == p[i-1] || '.' == p[i-1] {
+                            dp[i+1] = dp[i+1] || dp_prev[i+1];
+                        }
+                    }
+                    '.' => dp[i+1] = dp_prev[i],
+                    a if a == &ch => dp[i+1] = dp_prev[i],
+                    _ => dp[i+1] = false,
+                }
+            }
+        }
+        
+        dp[plen]
+    }
+}
