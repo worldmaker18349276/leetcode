4d461a02b0b1194b91109498baf17589020a130c
worldmaker <worldmaker18349276@gmail.com>
2022-02-22 18:26:11 +0800

    solve problem6 by iter

--- src/solutions/problem6.rs
+++ src/solutions/problem6.rs
@@ -50,3 +50,38 @@ impl Problem6 for Solution {
         res.into_iter().collect()
     }
 }
+
+struct SolutionIter;
+
+impl Problem6 for SolutionIter {
+    fn convert(s: String, num_rows: i32) -> String {
+        let num_rows = num_rows as usize;
+        if num_rows == 1 {
+            return s;
+        }
+
+        let mut res = Vec::new();
+        let s: Vec<_> = s.chars().collect();
+
+        for c in s.iter().step_by(num_rows * 2 - 2) {
+            res.push(c);
+        }
+
+        for i0 in 1..num_rows - 1 {
+            let iter1 = s.iter().skip(i0).step_by(num_rows * 2 - 2);
+            let mut iter2 = s.iter().skip(num_rows * 2 - 2 - i0).step_by(num_rows * 2 - 2);
+            for c in iter1 {
+                res.push(c);
+                if let Some(c) = iter2.next() {
+                    res.push(c);
+                }
+            }
+        }
+
+        for c in s.iter().skip(num_rows - 1).step_by(num_rows * 2 - 2) {
+            res.push(c);
+        }
+
+        res.into_iter().collect()
+    }
+}
