3ea578cf537bb025de9ece09898538ff9219e345
worldmaker <worldmaker18349276@gmail.com>
2022-02-22 16:52:01 +0800

    solve problem3 by iter

--- src/solutions/problem3.rs
+++ src/solutions/problem3.rs
@@ -31,3 +31,37 @@ impl Problem3 for Solution {
         length as i32
     }
 }
+
+struct SolutionIter;
+
+impl Problem3 for SolutionIter {
+    fn length_of_longest_substring(s: String) -> i32 {
+        use std::collections::HashSet;
+
+        if s.is_empty() {
+            return 0;
+        }
+
+        let bytes = s.as_bytes();
+        let mut start_iter = bytes.iter().enumerate();
+        let mut start = start_iter.next().unwrap();
+        let mut length = 0;
+        let mut seen = HashSet::new();
+
+        for (i, c) in bytes.iter().enumerate() {
+            if seen.contains(c) {
+                length = length.max(i - start.0);
+                while start.1 != c {
+                    seen.remove(start.1);
+                    start = start_iter.next().unwrap();
+                }
+                start = start_iter.next().unwrap();
+            } else {
+                seen.insert(c);
+            }
+        }
+
+        length = length.max(bytes.len() - start.0);
+        length as i32
+    }
+}
