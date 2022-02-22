8c602ac7fbbb299afa03426ebc3846b8ee7331a1
worldmaker <worldmaker18349276@gmail.com>
2022-02-22 16:54:46 +0800

    solve problem5 by iter

--- src/solutions/problem5.rs
+++ src/solutions/problem5.rs
@@ -30,6 +30,35 @@ impl Problem5 for Solution {
     }
 }
 
+struct SolutionIter;
+
+impl Problem5 for SolutionIter {
+    fn longest_palindrome(s: String) -> String {
+        let bytes = s.as_bytes();
+        let len = bytes.len();
+
+        let mut range = 0..=0;
+        let mut longest = 1;
+
+        for i in 0..len {
+            if len - i <= longest {
+                break;
+            }
+            for j in (i + longest..len).rev() {
+                let forward = bytes[i..(j + i + 2) / 2].iter();
+                let backward = bytes[(j + i) / 2..j + 1].iter().rev();
+                if forward.zip(backward).all(|(a, b)| a == b) {
+                    range = i..=j;
+                    longest = j - i + 1;
+                    break;
+                }
+            }
+        }
+
+        String::from(&s[range])
+    }
+}
+
 struct ManacherAlgorithm;
 
 impl Problem5 for ManacherAlgorithm {
