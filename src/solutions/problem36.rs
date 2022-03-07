12d374c1b1314293d6012b00fdaae76671605072
worldmaker <worldmaker18349276@gmail.com>
2022-03-07 18:58:00 +0800

    solve problem36 in a better way

--- src/solutions/problem36.rs
+++ src/solutions/problem36.rs
@@ -7,38 +7,46 @@ struct Solution;
 impl Problem36 for Solution {
     fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
         let init = 0b1111111111u16;
-        fn checker(acc: u16, val: &char) -> u16 {
+        fn check(acc: &mut u16, val: &char) -> bool {
             if *val == '.' {
-                return acc;
+                return true;
             }
-            let n = 0b1 << match *val {
-                '1' => 0,
-                '2' => 1,
-                '3' => 2,
-                '4' => 3,
-                '5' => 4,
-                '6' => 5,
-                '7' => 6,
-                '8' => 7,
-                '9' => 8,
-                _ => panic!(),
-            };
-            if acc & n != 0 {
-                acc ^ n
+            let n = 0b1
+                << match *val {
+                    '1' => 0,
+                    '2' => 1,
+                    '3' => 2,
+                    '4' => 3,
+                    '5' => 4,
+                    '6' => 5,
+                    '7' => 6,
+                    '8' => 7,
+                    '9' => 8,
+                    _ => panic!(),
+                };
+            if *acc & n != 0 {
+                *acc ^= n;
+                true
             } else {
-                0
+                false
             }
         }
-        board.iter().all(|row| row.iter().fold(init, checker) != 0)
-            && (0..9).all(|i| board.iter().map(|row| &row[i]).fold(init, checker) != 0)
-            && board[0..3].iter().flat_map(|row| row[0..3].iter()).fold(init, checker) != 0
-            && board[0..3].iter().flat_map(|row| row[3..6].iter()).fold(init, checker) != 0
-            && board[0..3].iter().flat_map(|row| row[6..9].iter()).fold(init, checker) != 0
-            && board[3..6].iter().flat_map(|row| row[0..3].iter()).fold(init, checker) != 0
-            && board[3..6].iter().flat_map(|row| row[3..6].iter()).fold(init, checker) != 0
-            && board[3..6].iter().flat_map(|row| row[6..9].iter()).fold(init, checker) != 0
-            && board[6..9].iter().flat_map(|row| row[0..3].iter()).fold(init, checker) != 0
-            && board[6..9].iter().flat_map(|row| row[3..6].iter()).fold(init, checker) != 0
-            && board[6..9].iter().flat_map(|row| row[6..9].iter()).fold(init, checker) != 0
+        let mut rows = [init, init, init, init, init, init, init, init, init];
+        let mut lines = [init, init, init, init, init, init, init, init, init];
+        let mut grids = [init, init, init, init, init, init, init, init, init];
+        for (i, row) in board.iter().enumerate() {
+            for (j, grid) in row.iter().enumerate() {
+                if !check(&mut rows[i], grid) {
+                    return false;
+                }
+                if !check(&mut lines[j], grid) {
+                    return false;
+                }
+                if !check(&mut grids[i / 3 * 3 + j / 3], grid) {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
