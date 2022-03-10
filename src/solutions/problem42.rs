15bd4096109d2127f5b3fb605a0e320460ccc9e8
worldmaker <worldmaker18349276@gmail.com>
2022-03-10 18:40:47 +0800

    solve problem42 faster

--- src/solutions/problem42.rs
+++ src/solutions/problem42.rs
@@ -24,3 +24,32 @@ impl Problem42 for Solution {
         volume
     }
 }
+
+struct SolutionFaster;
+
+impl Problem42 for SolutionFaster {
+    fn trap(height: Vec<i32>) -> i32 {
+        use std::cmp::Reverse;
+
+        let mut left: Vec<(usize, i32)> = Vec::new();
+        let mut volume = 0;
+        for (i, h) in height.iter().enumerate() {
+            let n = match left.binary_search_by_key(&Reverse(h), |(_, v)| Reverse(v)) {
+                Err(n) if n > 0 && n < left.len() => {
+                    volume += (i - 1 - left[n - 1].0) as i32 * (h - left[n].1);
+                    n
+                }
+                Err(n) | Ok(n) => {
+                    n
+                }
+            };
+            volume += left[n..]
+                .windows(2)
+                .map(|w| (i - 1 - w[0].0) as i32 * (w[0].1 - w[1].1))
+                .sum::<i32>();
+            left.drain(n..);
+            left.push((i, *h));
+        }
+        volume
+    }
+}
