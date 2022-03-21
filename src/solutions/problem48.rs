e6fb24e4bafc156cedabd10c6cdbfb246d33123d
worldmaker <worldmaker18349276@gmail.com>
2022-03-21 18:24:20 +0800

    solve problem48 faster but unsafe

--- src/solutions/problem48.rs
+++ src/solutions/problem48.rs
@@ -18,3 +18,32 @@ impl Problem48 for Solution {
         }
     }
 }
+
+struct SolutionFast;
+
+impl Problem48 for SolutionFast {
+    fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let n = matrix.len();
+        for i in 0..n / 2 {
+            for j in i..n - i - 1 {
+                let i_ = n - 1 - i;
+                let j_ = n - 1 - j;
+                let a = &matrix[i][j] as *const i32;
+                let b = &matrix[j_][i] as *const i32;
+                let c = &matrix[i_][j_] as *const i32;
+                let d = &matrix[j][i_] as *const i32;
+                unsafe {
+                    let a = a as *mut i32;
+                    let b = b as *mut i32;
+                    let c = c as *mut i32;
+                    let d = d as *mut i32;
+                    let temp = *a;
+                    *a = *b;
+                    *b = *c;
+                    *c = *d;
+                    *d = temp;
+                }
+            }
+        }
+    }
+}
