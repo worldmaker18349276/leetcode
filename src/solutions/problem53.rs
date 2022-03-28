58258dc4f5ff80afa1a14463a2e08153cec39957
worldmaker <worldmaker18349276@gmail.com>
2022-03-28 19:09:19 +0800

    solve problem53 using divide and conquer

--- src/solutions/problem53.rs
+++ src/solutions/problem53.rs
@@ -20,3 +20,26 @@ impl Problem53 for Solution {
         m
     }
 }
+
+struct SolutionDAC;
+
+impl Problem53 for SolutionDAC {
+    fn max_sub_array(nums: Vec<i32>) -> i32 {
+        // (max, left_ended_max, right_ended_max, sum)
+        fn go(nums: &[i32]) -> (i32, i32, i32, i32) {
+            match nums.len() {
+                1 => {
+                    let x = nums[0];
+                    (x, x, x, x)
+                }
+                n => {
+                    let n = n / 2;
+                    let (m1, l1, r1, s1) = go(&nums[..n]);
+                    let (m2, l2, r2, s2) = go(&nums[n..]);
+                    (m1.max(m2).max(r1 + l2), l1.max(s1 + l2), r2.max(s2 + r1), s1 + s2)
+                }
+            }
+        }
+        go(&nums).0
+    }
+}
