8124c1bd3afa044a4e75ae9692924912fb89b117
worldmaker <worldmaker18349276@gmail.com>
2022-04-08 18:38:28 +0800

    solve problem55 by calc

--- src/solutions/problem55.rs
+++ src/solutions/problem55.rs
@@ -26,3 +26,24 @@ impl Problem55 for Solution {
         perm.into_iter().map(|n| CHARS[n]).collect()
     }
 }
+
+struct SolutionCalc;
+
+impl Problem55 for SolutionCalc {
+    fn get_permutation(n: i32, k: i32) -> String {
+        static CHARS: &[char] = &['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        let n = n as usize;
+        let mut k = (k - 1) as usize;
+        let mut elems: Vec<_> = CHARS[..n].iter().collect();
+        let mut res = Vec::new();
+
+        let mut acc = 1;
+        let factorials: Vec<_> = (1..=n).map(|n| { acc *= n; acc }).collect();
+        for m in factorials.into_iter().rev().skip(1) {
+            res.push(elems.remove(k / m));
+            k %= m;
+        }
+        res.push(elems.pop().unwrap());
+        res.into_iter().collect()
+    }
+}
