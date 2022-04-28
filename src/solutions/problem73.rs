2aae6788e8d1ceb0ea666c1fbaa587ff9ebe0d47
worldmaker <worldmaker18349276@gmail.com>
2022-04-28 18:54:56 +0800

    solve problem73 in constant space

--- src/solutions/problem73.rs
+++ src/solutions/problem73.rs
@@ -32,3 +32,43 @@ impl Problem73 for Solution {
         }
     }
 }
+
+struct SolutionConst;
+
+impl Problem73 for SolutionConst {
+    fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+        let width = matrix[0].len();
+        let mut c = false;
+        {
+            for j in 0..width {
+                if matrix[0][j] == 0 {
+                    c = true;
+                }
+            }
+        }
+        for i in 1..matrix.len() {
+            let mut c = false;
+            for j in 0..width {
+                if matrix[i][j] == 0 {
+                    matrix[0][j] = 0;
+                    c = true;
+                }
+            }
+            if c {
+                matrix[i].fill(0);
+            }
+        }
+        {
+            for j in 0..width {
+                if matrix[0][j] == 0 {
+                    for line in matrix.iter_mut() {
+                        line[j] = 0;
+                    }
+                }
+            }
+            if c {
+                matrix[0].fill(0);
+            }
+        }
+    }
+}
